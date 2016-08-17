ef3f844a50f0140b08fe484ac4d63a721fbdc6a9
rishikant42 <rksbtp@gmail.com>
2016-08-17 11:20:14 +0530

    sorting in increasing order

---
+++ insertion-sort/test/insertion_sort/core_test.clj
@@ -0,0 +1,7 @@
+(ns insertion-sort.core-test
+  (:require [clojure.test :refer :all]
+            [insertion-sort.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
