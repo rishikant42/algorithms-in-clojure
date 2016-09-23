62d7d0067c7d930932bf232a217b6b2c4fda5776
rishikant42 <rksbtp@gmail.com>
2016-09-23 20:14:36 +0530

    improved code style

--- binary-search/src/binary_search/core.clj
+++ binary-search/src/binary_search/core.clj
@@ -3,19 +3,25 @@
 
 (defn bsearch
   [k list]
-  (loop [new-list list]
-    (let [size (count new-list)
-          mid (/ size 2)
-          current (nth new-list mid)
-          split-list (split-at mid new-list)
-          left-end (first split-list)
-          right-end (last split-list)]
-      (if (= current k)
-        (println "find")
-        (if (> current k)
-          (recur left-end)
-          (recur right-end))))))
+  (if (= (count list) 0)
+    (println "Element" k "is not present in list")
+    (let [mid (/ (count list) 2)
+          current (nth list mid)
+          split-list (split-at mid list)
+          left (first split-list)
+          right (last split-list)]
+      (cond (= k current) (println "Element" k "is present in list")
+            (< k current) (bsearch k left)
+            :else (bsearch k right)))))
 
 (defn -main
   [& args]
-  (println (bsearch 1 '(1 2 3 4 5 6 7 8 9 10))))
+  (bsearch 3 '(1 2 3 4 5 6 7 8 9 10)))
+
+
+;; (defn -main
+;;   [& args]
+;;   (let [a (map read-string args)
+;;         f (first a)
+;;         r (rest a)]
+;;     (bsearch f r)))
