14342f36ab72fb199236be5a359bd148804a4a70
rishikant42 <rksbtp@gmail.com>
2016-10-10 19:54:20 +0530

    fixed error in code

--- quick-sort/src/quick_sort/core.clj
+++ quick-sort/src/quick_sort/core.clj
@@ -2,27 +2,22 @@
   (:gen-class))
 
 
-(defn insert
-  [l]
-  (let [k (first l)
-        l (rest l)
-        t (concat 
-            (filter #(<= % k) l)
-            [k]
-            (filter #(> % k) l))]
-    (.indexOf t k)))
+;; (defn pivot-index
+;;   [l]
+;;   (let [k (first l)
+;;         l (rest l)
+;;         t (concat 
+;;             (filter #(<= % k) l)
+;;             [k]
+;;             (filter #(> % k) l))]
+;;     (.indexOf t k)))
 
-;; (defn quicksort
-;;   [arg]
-;;   (if (empty? arg)
-;;     nil
-;;     (let [x (insert arg)
-;;           y (nth arg x)
-;;           arg (rmve y arg)
-;;           [left right] (split-at x arg)]
-;;       (println y))))
-      ;; (concat (quicksort left) (quicksort right)))))
-
+(defn qsort [[pivot & xs]]
+  (when pivot
+    (let [smaller #(< % pivot)]
+      (lazy-cat (qsort (filter smaller xs))
+                [pivot]
+                (qsort (remove smaller xs))))))
 (defn -main
   [& args]
-  (println (quicksort '(3 8 6 4 7 5 0 2 1))))
+  (println (qsort '(3 8 6 4 7 5 0 2 1))))
