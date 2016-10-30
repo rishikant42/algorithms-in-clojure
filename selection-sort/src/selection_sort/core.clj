2cd42eec2e0a9916263b6bbde08df9b92fde3e68
rishikant42 <rksbtp@gmail.com>
2016-10-30 14:03:15 +0530

    changed remove procedure defination

--- selection-sort/src/selection_sort/core.clj
+++ selection-sort/src/selection_sort/core.clj
@@ -1,18 +1,31 @@
 (ns selection-sort.core
   (:gen-class))
 
+(defn remov
+  [x args]
+  (filter (fn [y] (not= x y)) args))
+
 (defn isort
   [arg]
   (loop [sorted-list []
          unsorted-list arg]
     (if (empty? unsorted-list)
       sorted-list
-      (recur (cons (apply min unsorted-list) sorted-list)
-             (remove (fn [x] (= x (apply min unsorted-list))) unsorted-list)))))
+      (let [minimum (apply min unsorted-list)]
+        (recur (cons minimum sorted-list)
+               (remov minimum unsorted-list))))))
 
 (defn -main
   [& args]
   (println (isort (map read-string args))))
 
+
+;; clojure primitive procedure, remove
+
 ;; => (remove (fn[x] (= x 3)) '(1 2 3 4 5))
+;; (1 2 4 5)
+
+;; above self defined procedure, remov
+
+;; => (remov 3 '(1 2 3 4 5))
 ;; (1 2 4 5)
